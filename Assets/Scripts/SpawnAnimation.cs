5676e7240e4616a4ee2e4644267e0e1f538f0cd5
teodor-vernica <teodor.vernica@gmail.com>
2022-04-25 17:13:11 +0200

    Updated UI. Added value legend.

--- Assets/Scripts/SpawnAnimation.cs
+++ Assets/Scripts/SpawnAnimation.cs
@@ -11,6 +11,7 @@ public class SpawnAnimation : MonoBehaviour
 
     private void Start()
     {
+        //Debug.Log("Starting.");
         MakeTransparent();
 
         //StartCoroutine(FadeIn3D(this.transform, 0, true, 10));
@@ -19,22 +20,25 @@ public class SpawnAnimation : MonoBehaviour
 
     private void Update()
     {
+
+        Debug.Log(GetComponent<Renderer>().material.color.a);
         if (fadeIn && material.color.a < 1f)
         {
+            
             material = GetComponent<Renderer>().material;
             color = material.color;
             //Debug.Log("time: " + Time.deltaTime + " fade: " + fadePerSecond * Time.deltaTime);
             material.color = new Color(color.r, color.g, color.b, color.a + (fadePerSecond * Time.deltaTime) );
 
-            //Debug.Log("a: " + color.a);
+            Debug.Log("a: " + color.a);
         }
-        else if(fadeOut && material.color.a > 0f)
+        /*else if(fadeOut && material.color.a > 0f)
         {
             material = GetComponent<Renderer>().material;
             color = material.color;
             //Debug.Log("time: " + Time.deltaTime + " fade: " + fadePerSecond * Time.deltaTime);
             material.color = new Color(color.r, color.g, color.b, color.a - (fadePerSecond * Time.deltaTime));
-        }
+        }*/
         else
         {
             fadeIn = false;
